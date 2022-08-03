4134d7cdfd7ab697ea7966d9b743f4d070849331
huzj <huzijiang@hqzhuanche.com>
2022-08-03 15:29:50 +0800

    init

--- src/main/java/com/xirm/demo/repository/CassandraRepositoryLocation.java
+++ src/main/java/com/xirm/demo/repository/CassandraRepositoryLocation.java
@@ -2,38 +2,15 @@ package com.xirm.demo.repository;
 
 import com.xirm.demo.entity.Location;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 import java.io.Serializable;
+
+
 
 /**
  * @author huzj
  * @version 1.0
  * @date 2022/8/2 10:19
  */
-public interface CassandraRepositoryLocation {
-
-    /**
-     *
-     * @param location
-     */
-    void save(Location location);
-
-    /**
-     *
-     */
-    void findAll();
-
-    /**
-     *
-     * @param location
-     */
-    void delete(Location location);
-
-    /**
-     *
-     * @param location
-     */
-    void update(Location location);
+public interface CassandraRepositoryLocation  extends CrudRepository<Location, Serializable> {
 
 }
